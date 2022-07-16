15a643d2bd9128478166f35491ebc7b8098bb396
Mahadevasairam <junnupatel5522@gmail.com>
2022-07-16 16:38:04 +0530

    Initial commit

--- src/Patterns/pattern7.java
+++ src/Patterns/pattern7.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class pattern7 {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.print("Enter number of Rows:");
+        System.out.print("Enter number of Rows: ");
         int n = sc.nextInt();
-        int i = n;
-        int j = (n*2)-4;
-        int row = 1;
-        while(row<=n){
-            if( row == 1 || row == n){
-                for(int star =0;star <n; star++)
-                    System.out.print("* ");
-                System.out.println();
-            }
-            else{
+        for (int row = 0; row < n; row++) {
+            if(row == 0 || row == n-1){  //print first and last line
+                for (int stars = 0; stars < n; stars++) {
+                        System.out.print("* ");
+                    }
+                }
+            else{ //print lines b/w first and last
                 System.out.print("* ");
-                for(int space = 0;space<j;space++)
-                    System.out.print(" ");
+                //print spaces block
+                for (int spaces = 0; spaces < n-2; spaces++) {
+                    System.out.print("  ");
+                    }
                 System.out.print("*");
-                System.out.println();
+                }
+            System.out.println();
             }
-           row++;
 
-        }
+
     }
+
 }
