57449efc91a98b6f47a30041cb288f4668af3604
Mahadevasairam <junnupatel5522@gmail.com>
2022-07-31 15:04:48 +0530

    Initial commit

--- src/InfyProblems/StringInfy.java
+++ src/InfyProblems/StringInfy.java
@@ -1,21 +1,19 @@
-package com.company;
+package InfyProblems;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class StringInfy {
     static String find(int n, String s, int m, List<Integer> a){
         StringBuilder sb = new StringBuilder(s);
-        for (int i = 0; i < a.size(); i++) {
-             int A = a.get(i); int k = 0;
-             String s1 = sb.substring(A-1,n-A+1);
-             StringBuilder ns = new StringBuilder(s1).reverse();
+        for (int A : a) {
+            int k = 0;
+            String s1 = sb.substring(A - 1, n - A + 1);
+            StringBuilder ns = new StringBuilder(s1).reverse();
             for (int j = 0; j < sb.length(); j++) {
-                if(j>=A-1 && j<n-A+1)
-                {
-                    sb.setCharAt(j,ns.charAt(k)) ;
+                if (j >= A - 1 && j < n - A + 1) {
+                    sb.setCharAt(j, ns.charAt(k));
                     k++;
                 }
             }
