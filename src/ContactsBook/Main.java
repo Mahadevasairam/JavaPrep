d4978ded0bd2c59a4870359258cd3ad232c95418
Mahadevasairam <junnupatel5522@gmail.com>
2022-07-31 11:13:09 +0530

    Initial commit

--- src/ContactsBook/Main.java
+++ src/ContactsBook/Main.java
@@ -1,10 +1,9 @@
-package Mobile;
+package ContactsBook;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    public static void printInstuct(){
+    public static void printInstruct(){
         System.out.println("0- To print Instructions");
         System.out.println("1- To add a Contact");
         System.out.println("2- To update a Contact");
@@ -18,21 +17,19 @@ public class Main {
         Contacts contact = new Contacts();
 
         boolean exit = false;
-        printInstuct();
+        printInstruct();
         Scanner s = new Scanner(System.in);
         while(!exit){
             int n = s.nextInt();
-            switch(n){
-                case 0: printInstuct();break;
-                case 1: contact.addContact(); break;
-                case 2: contact.updateContact(); break;
-                case 3: contact.removeContact(); break;
-                case 4: contact.fetchContacts(); break;
-                case 5: contact.searchContact(); break;
-                case 6: exit = true; break;
-                default :
-                    System.out.println("invalid choice");
-                    break;
+            switch (n) {
+                case 0 -> printInstruct();
+                case 1 -> contact.addContact();
+                case 2 -> contact.updateContact();
+                case 3 -> contact.removeContact();
+                case 4 -> contact.fetchContacts();
+                case 5 -> contact.searchContact();
+                case 6 -> exit = true;
+                default -> System.out.println("invalid choice");
             }
         }
     }
