e9febc5ca6c2f805f66df4ec8dc47bec1e65fa2c
Mahadevasairam <junnupatel5522@gmail.com>
2022-08-04 18:01:35 +0530

    Initial commit

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class HelloWorld {
      public static void main(String[] args){
-         String result = name();
+         String result = Name();
          System.out.println("Hello " + result);
     }
-    public static String name() {
+    public static String Name() {
         System.out.print("Enter your Name: ");
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        return name;
+        return sc.nextLine();
     }
 
 }
